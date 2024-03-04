ed948de50e03be26972373ebafcc36554a90353d
Hitesh Prajapati <hiteshprajapati96366@gmail.com>
2024-03-04 17:16:53 +0530

    second commit

--- photos/script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('nav');
-
-if (bar){
-    bar.addEventListener('click',()=>{
-        nav.classList.add('active');
-    })
-}
